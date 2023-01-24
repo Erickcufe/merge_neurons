a3fd90b4513aa72f530e22acb1135641b62c5035
Erickcufe <erickcufe94@gmail.com>
2023-01-24 13:25:23 -0600

    .

--- R/Anotacion_EC_neurons_seminario.R
+++ R/Anotacion_EC_neurons_seminario.R
@@ -12,71 +12,6 @@ library(scran)
 library(Seurat)
 library(SingleCellExperiment)
 
-so_neuron_merge <- readRDS("../Datos_scRNA/neurons_integrated/EC/datos_integrados_Anotados.rds")
-
-
-jpeg("images/neurons_EC_clusters_anotados_umap_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
-DimPlot(so_neuron_merge,reduction = "dens_map", cols = usecol("pal_unikn_pair", 16), label.size = 26, pt.size = 1)
-dev.off()
-
-VIP_neurons <- c("GAD", "VGLUT1", "VIP", "SOX6", "LHX6", "TAC3", "ADARB2", "PROX1")
-vip <- c("VIP", "PROX1", "ADARB2", "GAD1")
-pvalb <- c("PVALB", "SST", "VIP", "RELN", "NXPH1", "GAD1","ERBB4","SOX6","ADARB2")
-
-jpeg("images/neurons_EC_clusters_VIP_markers_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
-FeaturePlot(so_neuron_merge, features = vip, reduction = "dens_map",
-            cols = c("#EBE6E5","#EA0C3E"), label.size = 20)
-dev.off()
-
-jpeg("images/neurons_EC_clusters_VIP_sst_PV_markers_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
-FeaturePlot(so_neuron_merge, features = pvalb, reduction = "dens_map",
-            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
-dev.off()
-
-jpeg("images/neurons_EC_clusters_excitatory_markers_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
-FeaturePlot(so_neuron_merge, features = c("TBR1", "SATB2", "SLC17A7"), reduction = "dens_map",
-            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
-dev.off()
-
-
-jpeg("images/neurons_EC_clusters_ARTICULO_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
-FeaturePlot(so_neuron_merge, features = c("RORB", "CDH9"), reduction = "dens_map",
-            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
-dev.off()
-
-jpeg("images/neurons_EC_clusters_IILAYER_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
-FeaturePlot(so_neuron_merge, features = c("GPC5", "DCC", "PRKCA", "CNTN5"), reduction = "dens_map",
-            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
-dev.off()
-
-
-jpeg("images/neurons_EC_clusters_III_LAYER_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
-FeaturePlot(so_neuron_merge, features = c("NTNG1", "TRPS1"), reduction = "dens_map",
-            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
-dev.off()
-
-
-jpeg("images/neurons_EC_clusters_IV_V_LAYER_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
-FeaturePlot(so_neuron_merge, features = c("ZFPM2", "BRINP3", "FRMPD4", "NFIA", "PCSK5", "TLE4", "SEMA3E",
-                                          "CDH18", "LRP1B"), reduction = "dens_map",
-            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
-dev.off()
-
-so.renamed <- RenameIdents(so_neuron_merge, `0` = "Ex", `1` = "Ex", `2` = "Pv",
-                           `3` = "Vip", `4` = "Vip", `5`= "Sst",
-                           `6` = "Ex", `7`= "Ex", `8`= "Vip", `9`= "Sst",
-                           `10`= "Non-Vip", `11`= "Ex", `12`= "Ex",
-                           `13`= "Ex", `14`= "Non-Vip", `15`= "VIP",
-                           `16`= "Non-Vip", `17`= "Non-Vip", `18`= "Ex",
-                           `19` = "Ex", `20` = "Ex", `21` = "Ex")
-
-
-
-
-
-
-
-
 
 
 
@@ -151,3 +86,78 @@ so_neuron_merge[["dens_map"]] <- CreateDimReducObject(embeddings = reducedDim(sc
 Idents(so_neuron_merge) <- so_neuron_merge$Proplabels
 
 saveRDS(so_neuron_merge, "../Datos_scRNA/neurons_integrated/EC/datos_integrados_Anotados.rds")
+
+
+
+so_neuron_merge <- readRDS("../Datos_scRNA/neurons_integrated/EC/datos_integrados_Anotados.rds")
+
+
+jpeg("images/neurons_EC_clusters_anotados_umap_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
+DimPlot(so_neuron_merge,reduction = "dens_map", cols = usecol("pal_unikn_pair", 5), label.size = 26, pt.size = 1)
+dev.off()
+
+VIP_neurons <- c("GAD", "VGLUT1", "VIP", "SOX6", "LHX6", "TAC3", "ADARB2", "PROX1")
+vip <- c("VIP", "PROX1", "ADARB2", "GAD1")
+pvalb <- c("PVALB", "SST", "VIP", "RELN", "NXPH1", "GAD1","ERBB4","SOX6","ADARB2")
+
+jpeg("images/neurons_EC_clusters_VIP_markers_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
+FeaturePlot(so_neuron_merge, features = vip, reduction = "dens_map",
+            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
+dev.off()
+
+jpeg("images/neurons_EC_clusters_VIP_sst_PV_markers_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
+FeaturePlot(so_neuron_merge, features = pvalb, reduction = "dens_map",
+            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
+dev.off()
+
+jpeg("images/neurons_EC_clusters_excitatory_markers_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
+FeaturePlot(so_neuron_merge, features = c("TBR1", "SATB2", "SLC17A7"), reduction = "dens_map",
+            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
+dev.off()
+
+
+jpeg("images/neurons_EC_clusters_ARTICULO_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
+FeaturePlot(so_neuron_merge, features = c("RORB", "CDH9"), reduction = "dens_map",
+            cols = c("#EBE6E5","#EA0C3E"), label.size = 30, pt.size = 1)
+dev.off()
+
+jpeg("images/neurons_EC_clusters_IILAYER_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
+FeaturePlot(so_neuron_merge, features = c("GPC5", "DCC", "PRKCA", "CNTN5"), reduction = "dens_map",
+            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
+dev.off()
+
+
+jpeg("images/neurons_EC_clusters_III_LAYER_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
+FeaturePlot(so_neuron_merge, features = c("NTNG1", "TRPS1"), reduction = "dens_map",
+            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
+dev.off()
+
+
+jpeg("images/neurons_EC_clusters_IV_V_LAYER_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
+FeaturePlot(so_neuron_merge, features = c("ZFPM2", "BRINP3", "FRMPD4", "NFIA", "PCSK5", "TLE4", "SEMA3E",
+                                          "CDH18", "LRP1B"), reduction = "dens_map",
+            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
+dev.off()
+
+
+jpeg("images/neurons_EC_clusters_all_LAYERS_SEMINARIO.jpeg", units="in", width=15, height=10, res=300)
+FeaturePlot(so_neuron_merge, features = c("LTK", "FREM3", "GLP2R", "CARM1P1", "COL22A1"), reduction = "dens_map",
+            cols = c("#EBE6E5","#EA0C3E"), label.size = 26, pt.size = 1)
+dev.off()
+
+
+so.renamed <- RenameIdents(so_neuron_merge, `0` = "Ex", `1` = "Ex", `2` = "Pv",
+                           `3` = "Vip", `4` = "Vip", `5`= "Sst",
+                           `6` = "Ex", `7`= "Ex", `8`= "Vip", `9`= "Sst",
+                           `10`= "Non-Vip", `11`= "Ex", `12`= "Ex",
+                           `13`= "Ex", `14`= "Non-Vip", `15`= "VIP",
+                           `16`= "Non-Vip", `17`= "Non-Vip", `18`= "Ex",
+                           `19` = "Ex", `20` = "Ex", `21` = "Ex")
+
+
+
+
+
+
+
+
