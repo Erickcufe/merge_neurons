9c098e14b8be566e2e9f20489c7661c6482ac31c
Erickcufe <erickcufe94@gmail.com>
2023-04-17 20:39:44 -0600

    .

--- R/annotation_EC_neurons_from_SFG.R
+++ R/annotation_EC_neurons_from_SFG.R
@@ -1,4 +1,7 @@
 #Load data and convert to SCE
+library(dplyr)
+library(SingleCellExperiment)
+library(Seurat)
 so_neuron_merge <- readRDS(file.path("../Datos_scRNA/neurons_integrated/EC", "so_neuron_merge_all_ref_16PC_SFG.rds"))
 sce <- as.SingleCellExperiment(so_neuron_merge, assay = "RNA")
 colData(sce) <- as.data.frame(colData(sce)) %>%
@@ -7,18 +10,19 @@ colData(sce) <- as.data.frame(colData(sce)) %>%
 
 
 #Anotation with SingleR
-
 so_sfg <- readRDS("anotacion_Parcial_neuronas_neuronType.rds")
 
 sce_sfg <- as.SingleCellExperiment(so_sfg, assay = "RNA")
 
 
 library(SingleR)
-library(scater)
+# library(scater)
 
 pred <- SingleR(test = sce, ref = sce_sfg,
                 labels = colData(sce_sfg)$ident, assay.type.test=1,
-                BPPARAM= BiocParallel::MulticoreParam(7)) # 8 CPUs.
+                BPPARAM= BiocParallel::MulticoreParam(6)) # 8 CPUs.
 pred_modf <- pred[!duplicated(row.names(pred)),]
 colData(sce)$ProbLabels <- pred$labels
+jpeg("images/plotScore_annotated_EC_from_SFG.jpeg", units="in", width=10, height=10, res=300)
 plotScoreHeatmap(pred_modf)
+dev.off()
