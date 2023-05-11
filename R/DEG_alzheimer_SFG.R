a11c2b0a953d4ee9371f2298901b7a492841828b
Erickcufe <erickcufe94@gmail.com>
2023-05-11 10:51:28 -0600

    .

--- R/DEG_alzheimer_SFG.R
+++ R/DEG_alzheimer_SFG.R
@@ -207,7 +207,7 @@ get_pathfinder <- function(path="KEGG"){
 
     saveRDS(output_, paste0(cell_type, "_", path, ".rds"))
 
-    name_enrichment_chart <- paste0("images/", cell_type, "_","enrichment_chart.jpeg")
+    name_enrichment_chart <- paste0("images/", cell_type,"_",path, "_","enrichment_chart.jpeg")
     jpeg(name_enrichment_chart, units="in", width=25, height=17, res=300)
     print(enrichment_chart(output_, top_terms = 15) +
             theme(text = element_text(size = 25),
@@ -215,19 +215,19 @@ get_pathfinder <- function(path="KEGG"){
                   axis.text.y = element_text(size = 20)))
     dev.off()
 
-    name_term_gene_heatmap <- paste0("images/", cell_type, "_","gene_heatmap.jpeg")
+    name_term_gene_heatmap <- paste0("images/", cell_type,"_",path, "_",path, "_","gene_heatmap.jpeg")
     jpeg(name_term_gene_heatmap, units="in", width=25, height=17, res=300)
     print(term_gene_heatmap(output_, use_description = TRUE, num_terms = 15) +
             theme(text = element_text(size = 20)))
     dev.off()
 
-    name_term_gene_graph <- paste0("images/", cell_type, "_","gene_graph.jpeg")
+    name_term_gene_graph <- paste0("images/", cell_type,"_",path, "_","gene_graph.jpeg")
     jpeg(name_term_gene_graph, units="in", width=25, height=17, res=300)
     print(term_gene_graph(output_, use_description = TRUE, num_terms = 15) +
             theme(text = element_text(size = 20)))
     dev.off()
 
-    name_UpSet_plot <- paste0("images/", cell_type, "_","UpSet_plot.jpeg")
+    name_UpSet_plot <- paste0("images/", cell_type,"_",path, "_","UpSet_plot.jpeg")
     jpeg(name_UpSet_plot, units="in", width=25, height=17, res=300)
     print(UpSet_plot(output_, num_terms = 10, use_description = TRUE,
                      method = "barplot") +
