a8d070360eaed06e68f9ead9314927c797369bc8
Erickcufe <erickcufe94@gmail.com>
2023-05-31 18:16:53 -0600

    .

--- R/DEG_alzheimer_SFG.R
+++ R/DEG_alzheimer_SFG.R
@@ -493,3 +493,70 @@ table(all_up$cluster)
 readr::write_csv(all_up, "DEG_up_ADvsCt_perCelltype.csv")
 
 readr::write_csv(all_down, "DEG_down_ADvsCt_perCelltype.csv")
+
+
+suppressMessages(library(pathfindR))
+suppressMessages(library(ggplot2))
+
+
+select_genes_rorb <- readr::read_csv("deg_unicos_RORB.csv")
+
+rorb <- rorb[rorb$gene %in% select_genes_rorb$gene,]
+
+get_pathfinder <- function(f.markers){
+
+  path <- c("KEGG", "Reactome", "GO-All")
+  suppressMessages(library(pathfindR))
+  suppressMessages(library(ggplot2))
+
+  output_1 <- list()
+
+  for(i in path){
+    cell_type <- "RORB"
+    markers_ex1 <- data.frame(Gene.symbol = f.markers$gene,
+                              logFC = f.markers$avg_log2FC,
+                              adj.P.Val = f.markers$p_val_adj)
+
+    dir_path <- paste0("results_pathfinder","_", cell_type, "_",i)
+    output_ <- run_pathfindR(markers_ex1, output_dir = dir_path, gene_sets = i)
+
+    saveRDS(output_, paste0(cell_type, "_", i, ".rds"))
+
+    name_enrichment_chart <- paste0("images/", cell_type,"_",i, "_","enrichment_chart.jpeg")
+    jpeg(name_enrichment_chart, units="in", width=25, height=17, res=300)
+    print(enrichment_chart(output_, top_terms = 15) +
+            theme(text = element_text(size = 25),
+                  axis.text.x = element_text(size = 20),
+                  axis.text.y = element_text(size = 20)))
+    dev.off()
+
+    name_term_gene_heatmap <- paste0("images/", cell_type,"_",i, "_","gene_heatmap.jpeg")
+    jpeg(name_term_gene_heatmap, units="in", width=25, height=17, res=300)
+    print(term_gene_heatmap(output_, use_description = TRUE, num_terms = 15) +
+            theme(text = element_text(size = 20)))
+    dev.off()
+
+    name_term_gene_graph <- paste0("images/", cell_type,"_",i, "_","gene_graph.jpeg")
+    jpeg(name_term_gene_graph, units="in", width=25, height=17, res=300)
+    print(term_gene_graph(output_, use_description = TRUE, num_terms = 15) +
+            theme(text = element_text(size = 20)))
+    dev.off()
+
+    name_UpSet_plot <- paste0("images/", cell_type,"_",i, "_","UpSet_plot.jpeg")
+    jpeg(name_UpSet_plot, units="in", width=25, height=17, res=300)
+    print(UpSet_plot(output_, num_terms = 10, use_description = TRUE,
+                     method = "barplot") +
+            theme(text = element_text(size = 20),
+                  axis.text.x = element_text(size = 20),
+                  axis.text.y = element_text(size = 20)))
+    dev.off()
+  }
+
+
+
+  return(output_)
+}
+
+
+get_pathfinder(rorb)
+
