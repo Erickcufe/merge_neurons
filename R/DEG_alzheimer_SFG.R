17334eba0dec32a0185cc68c7e32c3daa52cd0cb
Erickcufe <erickcufe94@gmail.com>
2023-05-31 18:54:25 -0600

    .

--- R/DEG_alzheimer_SFG.R
+++ R/DEG_alzheimer_SFG.R
@@ -515,9 +515,9 @@ get_pathfinder <- function(f.markers){
     cell_type <- "RORB"
     markers_ex1 <- data.frame(Gene.symbol = f.markers$gene,
                               logFC = f.markers$avg_log2FC,
-                              adj.P.Val = f.markers$p_val_adj)
+                              adj.P.Val = f.markers$p_val)
 
-    dir_path <- paste0("results_pathfinder","_", cell_type, "_",i)
+    dir_path <- paste0("results_pathfinder","_", cell_type, "_",i, "_unicos")
     output_ <- run_pathfindR(markers_ex1, output_dir = dir_path, gene_sets = i)
 
     saveRDS(output_, paste0(cell_type, "_", i, ".rds"))
@@ -526,29 +526,29 @@ get_pathfinder <- function(f.markers){
     jpeg(name_enrichment_chart, units="in", width=25, height=17, res=300)
     print(enrichment_chart(output_, top_terms = 15) +
             theme(text = element_text(size = 25),
-                  axis.text.x = element_text(size = 20),
-                  axis.text.y = element_text(size = 20)))
+                  axis.text.x = element_text(size = 25),
+                  axis.text.y = element_text(size = 25)))
     dev.off()
 
     name_term_gene_heatmap <- paste0("images/", cell_type,"_",i, "_","gene_heatmap.jpeg")
     jpeg(name_term_gene_heatmap, units="in", width=25, height=17, res=300)
     print(term_gene_heatmap(output_, use_description = TRUE, num_terms = 15) +
-            theme(text = element_text(size = 20)))
+            theme(text = element_text(size = 25)))
     dev.off()
 
     name_term_gene_graph <- paste0("images/", cell_type,"_",i, "_","gene_graph.jpeg")
     jpeg(name_term_gene_graph, units="in", width=25, height=17, res=300)
     print(term_gene_graph(output_, use_description = TRUE, num_terms = 15) +
-            theme(text = element_text(size = 20)))
+            theme(text = element_text(size = 25)))
     dev.off()
 
     name_UpSet_plot <- paste0("images/", cell_type,"_",i, "_","UpSet_plot.jpeg")
     jpeg(name_UpSet_plot, units="in", width=25, height=17, res=300)
     print(UpSet_plot(output_, num_terms = 10, use_description = TRUE,
                      method = "barplot") +
-            theme(text = element_text(size = 20),
-                  axis.text.x = element_text(size = 20),
-                  axis.text.y = element_text(size = 20)))
+            theme(text = element_text(size = 25),
+                  axis.text.x = element_text(size = 25),
+                  axis.text.y = element_text(size = 25)))
     dev.off()
   }
 
